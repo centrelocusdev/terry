5e1121529e9e188ca16a99cfa2b3e0a27297f28d
Rupali Haldiya <rupali7487@gmail.com>
2023-04-08 12:15:13 +0530

    updating url

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import Cookies from "js-cookie";
 
-const API_URL_PROD = 'https://terry.thedelvierypointe.com'
-const API_URL_DEV = 'http://127.0.0.1:8085'
+const API_URL = 'https://terry.thedelvierypointe.com' //prod
+// const API_URL = 'http://127.0.0.1:8085'  //dev
 
 const headers = {
   headers: {
@@ -13,26 +13,26 @@ const headers = {
 
 //register
 export const register = async (formData) => {
-  const res = await axios.post(`${API_URL_DEV}/register`, formData)
+  const res = await axios.post(`${API_URL}/register`, formData)
   if(res.data.err) {
     toast.error(res.data.err)
     return
   } else {
     toast.success('user registered')
-    const user_res = await axios(`${API_URL_DEV}/user/${res.data.user.insertId}`)
+    const user_res = await axios(`${API_URL}/user/${res.data.user.insertId}`)
     Cookies.set('terri-auth', user_res.data.user[0].token)
     return res.data
   } 
 }
 
 export const login = async (formData) => {
-  const res = await axios.post(`${API_URL_DEV}/login`, formData)
+  const res = await axios.post(`${API_URL}/login`, formData)
   if(res.data.err) {
     toast.error(res.data.err)
     return
   } else {
     toast.success('user logged in')
-    const user_res = await axios(`${API_URL_DEV}/user?email=${formData.email}`)
+    const user_res = await axios(`${API_URL}/user?email=${formData.email}`)
     Cookies.set('terri-auth', user_res.data.user[0].token)
     return res.data
   }
@@ -45,7 +45,7 @@ export const logout = async () => {
 }
 
 export const getUserByToken = async () => {
-  const res = await axios.get(`${API_URL_DEV}/user-by-token/${Cookies.get('terri-auth')}`)
+  const res = await axios.get(`${API_URL}/user-by-token/${Cookies.get('terri-auth')}`)
   if(res.data?.err) {
     toast.error(res.data.err)
     return
@@ -55,7 +55,7 @@ export const getUserByToken = async () => {
 }
 
 export const insertContact = async (email, contact_id) => {
-  const res = await axios.post(`${API_URL_DEV}/insert-contact?email=${email}&contact_id=${contact_id}`)
+  const res = await axios.post(`${API_URL}/insert-contact?email=${email}&contact_id=${contact_id}`)
 
   if(res.data.err) {
     toast.error(res.data.err)
@@ -68,7 +68,7 @@ export const insertContact = async (email, contact_id) => {
 
 /**Invoice api */
 export const createContact = async (formData) => {
-  const res = await axios.post(`${API_URL_DEV}/create-contact`, formData)
+  const res = await axios.post(`${API_URL}/create-contact`, formData)
   if(res.data.err) {
     toast.error(res.data.err)
     return
@@ -79,7 +79,7 @@ export const createContact = async (formData) => {
 }
 
 export const createContactPerson = async (formData) => {
-  const res = await axios.post(`${API_URL_DEV}/create-contact-person`, formData)
+  const res = await axios.post(`${API_URL}/create-contact-person`, formData)
   if(res.data.err) {
     toast.error(res.data.err)
     return
@@ -90,7 +90,7 @@ export const createContactPerson = async (formData) => {
 }
 
 export const createItem = async (formData) => {
-  const res = await axios.post(`${API_URL_DEV}/create-item`, formData)
+  const res = await axios.post(`${API_URL}/create-item`, formData)
   if(res.data.err) {
     toast.error(res.data.err)
     return
@@ -101,7 +101,7 @@ export const createItem = async (formData) => {
 }
 
 export const createInvoice = async (formData) => {
-  const res = await axios.post(`${API_URL_DEV}/create-invoice`, formData)
+  const res = await axios.post(`${API_URL}/create-invoice`, formData)
   if(res.data.err) {
     toast.error(res.data.err)
     console.log(res.data)
@@ -113,7 +113,7 @@ export const createInvoice = async (formData) => {
 }
 
 export const getItems = async () => {
-  const res = await axios.get(`${API_URL_DEV}/items`)
+  const res = await axios.get(`${API_URL}/items`)
   if(res.data.err) {
     toast.error(res.data.err)
     return
@@ -123,7 +123,7 @@ export const getItems = async () => {
 }
 
 export const getContactPersons = async (conatct_id) => {
-  const res = await axios.get(`${API_URL_DEV}/contact-persons/${conatct_id}`)
+  const res = await axios.get(`${API_URL}/contact-persons/${conatct_id}`)
   if(res.data.err) {
     toast.error(res.data.err)
     return
@@ -133,7 +133,7 @@ export const getContactPersons = async (conatct_id) => {
 }
 
 export const getInvoices = async () => {
-  const res = await axios.get(`${API_URL_DEV}/invoices`)
+  const res = await axios.get(`${API_URL}/invoices`)
   if(res.data.err) {
     toast.error(res.data.err)
     return
